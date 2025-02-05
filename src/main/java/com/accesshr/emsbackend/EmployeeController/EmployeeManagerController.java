91031b47d542c6131e0acb38f6b7654f9854261b
MTL1016 <vedasamhithagopalam@gmail.com>
2025-02-05 11:47:25 +0530

    New

--- src/main/java/com/accesshr/emsbackend/EmployeeController/EmployeeManagerController.java
+++ src/main/java/com/accesshr/emsbackend/EmployeeController/EmployeeManagerController.java
@@ -25,7 +25,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/employeeManager")
-@CrossOrigin(origins = "https://dobby17.azurewebsites.net") // Adjust as needed for your frontend
+@CrossOrigin(origins = "http://localhost:3000") // Adjust as needed for your frontend
 public class EmployeeManagerController {
 
     @Value("${azure.storage.connection-string}")
